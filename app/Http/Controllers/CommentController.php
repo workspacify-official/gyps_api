487929fc894718bf37a77a8df4b18f9552d95376
md sawkat hossain <saikat@workspacify.com>
2022-06-06 18:35:46 +0600

    comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,7 +17,28 @@ class CommentController extends Controller
     {
         if($request->isMethod('post')){
 
-            
+            $datall = $request->all();
+            $rules = [
+                'post_id' => 'required',
+                'comment' => 'required'
+            ];
+            $customesmesg = [
+                'post_id.required' => 'Post ID is required',
+                'comment.required' => 'Comment is required',
+            ];
+             
+            $validations  = Validator::make($datall, $rules, $customesmesg);
+            if($validations->fails()){
+                return response()->json($validations->errors(), 433);
+            }
+
+            $data = new Comments();
+            $data->post_id = $request->post_id;
+            $data->comment = $request->comment;
+            $data->parent_id = $request->parent_id;
+            $data->user_id = Auth::user()->id;
+            $data->save();
+            return response()->json(['status' => 'success', 'messages' => 'Data hass saved success!'], 200);
 
 
         }else{
