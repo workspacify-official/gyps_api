7a21831067013988342e38958f37c0212e442e65
md sawkat hossain <saikat@workspacify.com>
2022-06-13 21:29:48 +0600

    updat

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
             //->withCount('followingcheck')->where('user_id', $user_id)
             ->withCount(['followingcheck', 
                         'followingcheck' => function ($query) {
-                            $query->where('user_id', '>', $user_id);
+                            $query->where('followers.user_id', '>', $user_id);
                         }])
             ->orderBy('id', 'DESC')
             ->paginate(5);
